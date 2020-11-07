67e2b87a657929d0ace30b1a4014bc76e6304d91
Elam Day <elam.day.friedland@gmail.com>
2020-11-06 23:00:19 -0800

    whoops switched h/j and k/l

--- config.h
+++ config.h
@@ -123,10 +123,10 @@ static Key keys[] = {
 	TAGKEYS(			XK_7,		6)
 	TAGKEYS(			XK_8,		7)
 	TAGKEYS(			XK_9,		8)
-	{ MODKEY|ShiftMask,                       XK_l, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,                       XK_h, focusmon,       {.i = +1 } },
-	{MODKEY|ShiftMask, XK_k, tagmon,         {.i = +1 } },
-	{MODKEY|ShiftMask, XK_j, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,                       XK_k, focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,                       XK_j, focusmon,       {.i = +1 } },
+	{MODKEY|ShiftMask, XK_l, tagmon,         {.i = +1 } },
+	{MODKEY|ShiftMask, XK_h, tagmon,         {.i = +1 } },
 	{ MODKEY,			XK_0,		view,		{.ui = ~0 } },
 	{ MODKEY|ShiftMask,		XK_0,		tag,		{.ui = ~0 } },
 	{ MODKEY,			XK_minus,	spawn,		SHCMD("pamixer --allow-boost -d 5; kill -44 $(pidof dwmblocks)") },
